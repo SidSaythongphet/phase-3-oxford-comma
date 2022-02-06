4522221789030f0f01d65ab9fe34a4a58ae33007
Sid Saythongphet <sid.saythongphet@gmail.com>
2022-02-06 15:32:48 -0500

    commit tree 8ab3f555e8590522e2155ae25da04a2a99bc5fc6

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,14 @@
 def oxford_comma(array)
-
+    string = ''
+    case array.length
+    when 1 then array.join
+    when 2 then array.join(' and ') 
+    when 3 then  string << array[0] + ', ' + array[1] + ', and ' + array[2]
+    else  array[0...-1].join(', ') + ', and ' + array.last
+    end
 end
+
+oxford_comma(["kiwi"])
+oxford_comma(["kiwi", "durian"])
+oxford_comma(["fiddleheads", "okra", "kohlrabi"])
+oxford_comma(["kiwi", "durian", "starfruit", "mangos", "dragon fruits"])
